7b950cb5a5a287b74e58a99f52a91c1563913c11
AgungNaya <geknaya54@gmail.com>
2025-07-01 14:31:10 +0800

    My first appium project with WDIO

---
+++ tests/appdemo.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('API Demo Testing', () => {
+    it('Should open the App', async () => {
+        console.log('Aplikasi terbuka');
+        expect(true).to.equal(true);
+    });
+});
